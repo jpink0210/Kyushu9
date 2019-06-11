f8c8af8020f00886ac4a55c17faac78a45611837
pinpin jiao <jpink0210@gmail.com>
2019-06-12 00:54:04 +0800

    6/18 done

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var app = new Vue({
 	el: "#app",
 	data: {
 		chosen: 0,
+		isDetail: true,
 		all: [
 			{
 				date: "6/18",
@@ -22,8 +23,28 @@ var app = new Vue({
 					{ time: "",sth: ["晚餐後行程未定"],cost: 0, },
 				],
 				plays: [
-
-				]
+					{
+						place: "博多車站",
+						views:[
+							{where: "天神地下街", ctn: "10:00～20:00" , link:"https://gojp.tw/2015-12-30-163/", cmmt:"中餐在這好了", img:"https://img.gojp.tw/pixnet/abbb0162157c1df4220e9c9c9da72a5d.jpg"},
+						]
+					},
+					{
+						place: "太宰府天滿宮",
+						views:[
+							{where: "太宰府天滿宮表參道", ctn: "" , link:"", cmmt:"就跟淺草一樣/梅枝餅", img:"https://dg63mrbespfka.cloudfront.net/2017/07/sando1-430x265.jpg"},
+							{where: "太宰府天滿宮", ctn: "" , link:"https://tw.bring-you.info/dazaifushi", cmmt:"花不多時間吧", img:"https://tw.bring-you.info/imgs/2017/12/dazaifushi-16.jpg"},
+							{where: "暖暮拉麵", ctn: "" , link:"", cmmt:"我們有吃過了", img:"https://pic.pimg.tw/nikitarh/1516460678-3957683917_n.jpg"},
+						]
+					},
+					{
+						place: "博多-天神屋台",
+						views:[
+							{where: "中洲屋台", ctn: "福岡博多天神的屋台，就類似於台灣夜市的小攤販一樣， 佇立在夜晚燈紅的街道上，擁有200家店舖營業，是許多福岡縣民或是觀光客夜晚覓食的好去處" , link:"https://zh.compathy.net/magazine/2017/07/05/fukuokatenjinyatai/", cmmt:"晚餐", img:"https://compathy-magazine-assets.compathy.net/uploads/2017/07/17417935940_e876f16f9b_z-640x427.jpg"},
+							{where: "博多運河城", ctn: "免稅店/10-21" ,img:"https://dg63mrbespfka.cloudfront.net/2018/08/Fukuoka_Spot11-430x250.JPG", link:"", cmmt:""},
+						]
+					},
+				],
 			},
 			{
 				date: "6/19",
@@ -39,7 +60,15 @@ var app = new Vue({
 					{ time: "19:30",sth: ["飯店開車到稻佐山夜景"],cost: 25, },
 					{ time: "21:00",sth: ["回飯店泡溫泉	"],cost: 0, },
 					{ time: "21:30",sth: ["飯店免費拉麵宵夜	"],cost: 0, },
-				]
+				],
+				plays: [
+					{
+						place: "",
+						views:[
+							{where: "", ctn: "" , link:"", cmmt:"", img:""},
+						]
+					},
+				],
 
 			},
 			{
@@ -55,7 +84,15 @@ var app = new Vue({
 					{ time: "13:30",sth: ["走路15分鐘熊本城	","城采苑，熊本熊辦公室	"],cost: 0,},
 					{ time: "18:00",sth: ["晚餐吃豬排飯 ","http://gtchengintpep.pixnet.net/blog/post/221534610-%E6%9C%89%E4%B8%80%E7%A8%AE%E5%AF%A6%E5%8A%9B%E5%8F%AB%22%E5%BF%85%E5%90%83%22%E6%97%A5%E6%9C%AC%E7%86%8A%E6%9C%AC%E3%80%90%E9%B9%BF%E5%85%92%E5%B3%B6%E5%85%AD%E7%99%BD%E9%BB%91	1800"],cost: 0,},
 					{ time: "",sth: ["晚餐後行程未定"],cost: 0,},
-				]
+				],
+				plays: [
+					{
+						place: "",
+						views:[
+							{where: "", ctn: "" , link:"", cmmt:"", img:""},
+						]
+					},
+				],
 
 			},
 
@@ -69,7 +106,15 @@ var app = new Vue({
 					{ time: "14:00",sth: ["往別府地獄出發  ","大分県別府市鉄輪５５９−１","七個地獄中挑三個","https://candicecity.com/31830/","看看明礬湯之里有沒有要去 ","https://gooshuo.tw/article/782"],cost: 0,},
 					{ time: "18:00",sth: ["晚餐地獄蒸鉄輪工坊 map code:", "46 523 435*17"],cost: 0,},
 					{ time: "19:00",sth: ["往飯店出發"],cost: "879-1305, 杵築市, 山香町倉成3003番",},
-				]
+				],
+				plays: [
+					{
+						place: "",
+						views:[
+							{where: "", ctn: "" , link:"", cmmt:"", img:""},
+						]
+					},
+				],
 			},
 			{
 				date: "6/22",
@@ -83,8 +128,15 @@ var app = new Vue({
 					{ time: "15:00",sth: ["開車到民宿"],cost: 0,},
 					{ time: "16:30",sth: ["放好行李後加油還車"],cost: 1500,},
 					{ time: "17:00",sth: ["前往吃飯地點"],cost: 3000,},
-				
-				]
+				],
+				plays: [
+					{
+						place: "",
+						views:[
+							{where: "", ctn: "" , link:"", cmmt:"", img:""},
+						]
+					},
+				],
 
 			},
 		],
@@ -94,7 +146,12 @@ var app = new Vue({
 	},	
 	methods: {
 		changeDay: function(day){
-			this.chosen = day;
+			if(day == this.chosen){
+				this.isDetail = !this.isDetail;
+			}else{
+				this.isDetail = true;
+				this.chosen = day;
+			}
 		}
 	},
 	computed:{
